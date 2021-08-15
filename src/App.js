463ae93e1810b1db4d9a36de9cb0daa72d84154b
Alexandr-Grigoryev <grigory98ev@gmail.com>
2021-08-15 21:52:01 +0300

    Profile-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './user.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <Profile userData={user} />;
 }
-
-export default App;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import s from './Profile.module.css';
+
+function Profile({ userData }) {
+  console.log(userData.name);
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img
+          src={userData.avatar}
+          alt="Аватар пользователя"
+          className={s.avatar}
+        />
+        <p className={s.name}>{userData.name}</p>
+        <p className={s.tag}>{userData.tag}</p>
+        <p className={s.location}>{userData.location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li>
+          <span className={s.label}>Followers</span>
+          <span class={s.quantity}>{userData.stats.followers}</span>
+        </li>
+        <li>
+          <span className={s.label}>Views</span>
+          <span class={s.quantity}>{userData.stats.views}</span>
+        </li>
+        <li>
+          <span className={s.label}>Likes</span>
+          <span class={s.quantity}>{userData.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+Profile.propTypes = {
+  userData: PropTypes.object.isRequired,
+};
+
+export default Profile;
